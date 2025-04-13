745c25c323589009250c490318d7b6b4ba80062b
yumiyulili <masha.ava04@gmail.com>
2025-04-13 18:52:45 +0300

    genpass

---
+++ genpass.js
@@ -0,0 +1,18 @@
+const pass = document.getElementById("password");
+
+function genPassword() {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz_-()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = 12;
+    let password = "";
+ for (let i = 0; i <= passwordLength; i++) {
+   let randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+ function copyPassword() {
+    const copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");  
+  }
